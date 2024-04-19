1754cb210e3bb145dab4347a40b15c87600402b8
ClaPascarella <claudiopascarella1996@gmail.com>
2024-04-19 16:53:53 +0200

    file chtml e js

---
+++ main.js
@@ -0,0 +1,28 @@
+for (let i= 1; i<= 100; i++) {
+
+    console.log (i)
+    
+    const numOne =i;
+    const numTwo = 3;
+
+    if (numOne % numTwo == 0) {
+        console.log(`Fizz`);
+    }
+
+    
+    
+
+    const numThree = i;
+    const numFour = 5;
+    if (numThree % numFour == 0) {
+       console.log(`Buzz`);
+    }
+
+   
+
+    
+    
+    
+
+}
+
